97f365e7257e3bca7167d565af94e0b882674d4c
Tomoro <tomoro0726@gmail.com>
2022-06-05 16:21:09 +0900

    first commit

---
+++ index1.py
@@ -0,0 +1,26 @@
+#書記配列
+vertical=["1","2","3","4","5","6","7","8"]
+beside=["a","b","c","d","e","f","g","h"]
+all=[]
+#マス目定義
+for i in range(len(vertical)):
+  for f in range(len(beside)):
+    add=[]
+    add.append(beside[f]+vertical[i])
+    add.append(0)
+    all.append(add)
+    f=f+1
+  i=i+1
+#マス目呼び出し関数
+def cell(a,b):
+    x=beside.index(a)+(int(b)-1)*len(vertical)
+    return x
+#初期位置更新
+all[cell("d","4")][1]=1
+all[cell("e","4")][1]=2
+all[cell("d","5")][1]=2
+all[cell("e","5")][1]=1
+
+
+
+print(all)
