52ac3af7026c001b5b13a2c3185e96eef1584886
Tomoro <tomoro0726@gmail.com>
2022-06-05 16:33:37 +0900

    判定作成中

--- index1.py
+++ index1.py
@@ -1,4 +1,7 @@
 #書記配列
+from ast import For
+
+
 vertical=["1","2","3","4","5","6","7","8"]
 beside=["a","b","c","d","e","f","g","h"]
 all=[]
@@ -20,7 +23,14 @@ all[cell("d","4")][1]=1
 all[cell("e","4")][1]=2
 all[cell("d","5")][1]=2
 all[cell("e","5")][1]=1
-
-
-
-print(all)
+#打つことができる場所
+def next(a,b):
+  x=[]
+  for i in range(len(all)):
+    if all[i]==0:
+      print("可能性あり")
+    else:
+      print("可能性なし")
+    
+    
+        
